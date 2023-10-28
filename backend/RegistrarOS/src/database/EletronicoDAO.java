d646a4a4e8a7966cf4654d15714b5a25845e5a0a
JerffersonJunior <jerfferson.junior@ufms.br>
2023-10-28 16:46:41 -0400

    update: adiciona código de EletronicoDAO

--- backend/RegistrarOS/src/database/EletronicoDAO.java
+++ backend/RegistrarOS/src/database/EletronicoDAO.java
@@ -1,5 +1,47 @@
 package database;
 
-public class EletronicoDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import model.Eletronico;
 
+public class EletronicoDAO {
+	
+	public Eletronico buscaEletronico(String serie) throws SQLException, ConectException {
+        Connection conexao = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        
+        try {
+            conexao = Conect.getConect();
+
+            String sql = "SELECT * FROM clientes WHERE telefone = ?";
+            stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, telefone);
+
+            rs = stmt.executeQuery();
+            Cliente cli = null;
+            
+            if (rs.next()) {
+                cli = new Cliente(
+                    rs.getString("nome"),
+                    rs.getString("endereco"),
+                    rs.getString("telefone")
+                );
+            }
+
+            return cli;
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conexao != null) {
+                conexao.close();
+            }
+        }
+    }
 }
